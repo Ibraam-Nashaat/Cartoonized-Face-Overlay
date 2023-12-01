1f600f2ee0216fb032f506e26b82b81351cc1669
Ibraam Nashaat <ibraam.nashaat2@gmail.com>
2023-12-01 14:48:51 +0200

    Changed some haarlikefeautues class methods into private methods

--- haarlikefeautures.py
+++ haarlikefeautures.py
@@ -25,7 +25,7 @@ class HaarLikefeatures:
 
         self.utils = Utils()
 
-    def get_sum_in_rectangle(self, integral_image, start_row, start_col, w, h):
+    def __get_sum_in_rectangle(self, integral_image, start_row, start_col, w, h):
         """
         Gets the sum of pixels in rectangle
 
@@ -52,7 +52,7 @@ class HaarLikefeatures:
             integral_image[start_row, start_col+w]
         return int(sum)
 
-    def get_feauture_value(self, integral_image, feauture_type, start_row, start_col, w, h):
+    def __get_feauture_value(self, integral_image, feauture_type, start_row, start_col, w, h):
         """
         Gets the value of a cerain haar feauture type
 
@@ -76,49 +76,53 @@ class HaarLikefeatures:
         black = 0
         white = 0
         if feauture_type == HaarFeautureTypes.TWO_HORIZONTAL:
-            black = self.get_sum_in_rectangle(
+            black = self.__get_sum_in_rectangle(
                 integral_image, start_row, start_col, w, h/2)
-            white = self.get_sum_in_rectangle(
+            white = self.__get_sum_in_rectangle(
                 integral_image, start_row+h/2, start_col, w, h/2)
         elif feauture_type == HaarFeautureTypes.TWO_VERTICAL:
-            white = self.get_sum_in_rectangle(
+            white = self.__get_sum_in_rectangle(
                 integral_image, start_row, start_col, w/2, h)
-            black = self.get_sum_in_rectangle(
+            black = self.__get_sum_in_rectangle(
                 integral_image, start_row, start_col + w/2, w/2, h)
         elif feauture_type == HaarFeautureTypes.THREE_HORIZONTAL:
-            white = self.get_sum_in_rectangle(
+            white = self.__get_sum_in_rectangle(
                 integral_image, start_row, start_col, w, h/3) + \
-                self.get_sum_in_rectangle(
+                self.__get_sum_in_rectangle(
                     integral_image, start_row+(2*h/3), start_col, w, h/3)
-            black = self.get_sum_in_rectangle(
+            black = self.__get_sum_in_rectangle(
                 integral_image, start_row+(h/3), start_col, w, h/3)
         elif feauture_type == HaarFeautureTypes.THREE_VERTICAL:
-            white = self.get_sum_in_rectangle(
+            white = self.__get_sum_in_rectangle(
                 integral_image, start_row, start_col, w/3, h) + \
-                self.get_sum_in_rectangle(
+                self.__get_sum_in_rectangle(
                     integral_image, start_row, start_col+(2*w/3), w/3, h)
-            black = self.get_sum_in_rectangle(
+            black = self.__get_sum_in_rectangle(
                 integral_image, start_row, start_col+(w/3), w/3, h)
         elif feauture_type == HaarFeautureTypes.FOUR_DIAGONAL:
-            white = self.get_sum_in_rectangle(
+            white = self.__get_sum_in_rectangle(
                 integral_image, start_row, start_col, w/2, h/2) + \
-                self.get_sum_in_rectangle(
+                self.__get_sum_in_rectangle(
                     integral_image, start_row+h/2, start_col+w/2, w/2, h/2)
-            black = self.get_sum_in_rectangle(
+            black = self.__get_sum_in_rectangle(
                 integral_image, start_row+w/2, start_col, w/2, h/2) + \
-                self.get_sum_in_rectangle(
+                self.__get_sum_in_rectangle(
                     integral_image, start_row+h/2, start_col, w/2, h/2)
 
         feauture_value = white-black
         return feauture_value
 
-    def extract_features(self, original_image, width, height):
+    def extract_features(self, original_image, start_row, start_col, width, height):
         """
         Extracts the features from the sliding window that slides over the 
         original image
 
         Parameters:
         original_image: np.array
+        start_row: int
+              start row of the sliding window
+        start_col: int
+              start column of the sliding window
         width: int
               width of the detection window
         height: int
@@ -134,15 +138,15 @@ class HaarLikefeatures:
             wndx, wndy = self.haar_window[haar_type]
             for w in range(wndx, width+1, wndx):
                 for h in range(wndy, height+1, wndy):
-                    for x in range(0, width-w+1):
-                        for y in range(0, height-h+1):
+                    for x in range(start_col, start_col+width-w+1):
+                        for y in range(start_row, start_row+height-h+1):
                             features = np.append(
                                 features, [[haar_type, x, y, w, h]], axis=0)
 
         features_values = np.zeros((len(features)))
         integral_image = self.utils.get_integral_image(original_image)
         for i in range(len(features)):
-            features_values[i] = self.get_feauture_value(
+            features_values[i] = self.__get_feauture_value(
                 integral_image,
                 HaarFeautureTypes(features[i][0]),
                 features[i][1],
@@ -152,13 +156,3 @@ class HaarLikefeatures:
             )
 
         return features_values
-
-
-# utils = Utils()
-# integral_image = utils.get_integral_image(
-#     np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-# print(integral_image)
-haar_like_feautures = HaarLikefeatures()
-# print(feauture.get_sum_in_rectangle(integral_image, 0, 0, 2, 2))
-print(haar_like_feautures.extract_features(
-    np.array([[8, 2, 3, 7, 4, 9, 9, 10], [4, 5, 6, 1, 1, 3, 5, 6], [7, 8, 9, 8, 9, 8, 9, 8], [7, 8, 9, 8, 9, 8, 1, 9]]), 3, 3))
