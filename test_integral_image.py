ff3b510bc9e25585fd98c46fd10e32e621b89234
Ibraam Nashaat <ibraam.nashaat2@gmail.com>
2023-12-08 20:05:35 +0200

    Added integral image and haar like features tests

---
+++ test_integral_image.py
@@ -0,0 +1,17 @@
+import skimage.io as io
+from skimage.color import rgb2gray
+from utils import *
+from skimage.transform import integral_image as sk_integral_image
+
+
+def test_integral_image():
+    img = io.imread("Images/hermoine.jpg")
+    img_gray = rgb2gray(img)
+    utils = Utils()
+    custom_integral_image = np.array(
+        utils.get_integral_image(img_gray)[1:, 1:], dtype=int)
+
+    sk_integral = np.array(sk_integral_image(img_gray), dtype=int)
+
+    assert np.array_equal(custom_integral_image,
+                          sk_integral), "The integral images do not match!"
